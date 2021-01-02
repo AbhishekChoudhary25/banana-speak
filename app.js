470e4ca25a621d7caf8999672881cb49c5183091
Abhishek Choudhary <abhich2025@gmail.com>
2021-01-02 11:51:42 +0530

    Completed

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");  
-var txtInput = document.querySelector("txt-input");
-btnTranslate.addEventListener("click",function(){
-    console.log("Clicked");
-    
-})
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(text){
+    return url + "?" + "text=" + text;
+}
+console.log(txtInput);
+
+function errorHandler(error){
+    console.log("error" + error);
+}
+
+function clickHandler(){
+    // outputDiv.innerText = "asasas" + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslatedUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        outputDiv.innerText = json.contents.translated;
+    })
+    .catch(errorHandler); 
+}
+
+btnTranslate.addEventListener("click",clickHandler);
+
+ 
